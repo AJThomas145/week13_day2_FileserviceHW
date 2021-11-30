f2eb78ad5b95ef376920781b465f9ca57e699622
Andrew Thomas <ajthomas145@gmail.com>
2021-11-30 16:16:58 +0000

    initial setup

---
+++ src/test/java/com/codeclan/day2HW/Fileservice/FileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.day2HW.Fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
